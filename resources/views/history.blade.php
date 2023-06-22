02ad7dcd8fe1520545eb4dab43caced7440f45e7
ichsan <ichsanseanaldi10@gmail.com>
2023-06-22 14:25:53 +0700

    for to foreach

--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -19,35 +19,35 @@
                     <div class="col col-1">Ongkir</div>
                     <div class="col col-3">Total Bayar</div>
                 </li>
-                @for($i=0; $i<count($data)-1;$i++)
+                @foreach ($data as $cust)
                     <li class="table-row">
-                        <div class="col col-1">{{ $i+1 }}</div>
-                        <div class="col col-4">{{ $data[$i]->nomor_transaksi }}</div>
-                        <div class="col col-3">{{ date('d-M-Y', strtotime($data[$i]->tanggal_transaksi)) }}</div>
-                        <div class="col col-2">{{ $data[$i]->nama_cust }}</div>
-                        <div class="col col-1">{{ $data[$i]->jumlah_barang }}</div>
+                        <div class="col col-1">{{ $loop->iteration }}</div>
+                        <div class="col col-4">{{ $cust->nomor_transaksi }}</div>
+                        <div class="col col-3">{{ date('d-M-Y', strtotime($cust->tanggal_transaksi)) }}</div>
+                        <div class="col col-2">{{ $cust->nama_cust }}</div>
+                        <div class="col col-1">{{ $cust->jumlah_barang }}</div>
                         <div class="col col-4">
                             <span class="number-format">
-                                {{ $data[$i]->subtotal }}
+                                {{ $cust->subtotal }}
                             </span>
                         </div>
                         <div class="col col-4">
                             <span class="number-format">
-                                {{ $data[$i]->diskon_coupon }}
+                                {{ $cust->diskon_coupon }}
                             </span>
                         </div>
                         <div class="col col-1">
                             <span class="number-format">
-                                {{ $data[$i]->ongkir }}
+                                {{ $cust->ongkir }}
                             </span>
                         </div>
                         <div class="col col-3">
                             <span class="number-format field-collect">
-                                {{ $data[$i]->total_bayar }}
+                                {{ $cust->total_bayar }}
                             </span>
                         </div>
                     </li>
-                @endfor
+                @endforeach
             </ul>
         </div>
         <div style="text-align:end; margin-top:30px">
