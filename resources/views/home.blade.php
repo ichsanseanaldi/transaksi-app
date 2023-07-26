6d26cb4a3801d802fd01ef08fb6dcbbbc8ea90b4
ichsan <ichsanseanaldi10@gmail.com>
2023-07-26 20:31:46 +0700

    added home view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+@extends('main')
+
+@section('child')
+
+    <noscript>Turn on Javascript Please !</noscript>
+
+    <div class="section-wrapper flex-center center m-20-ud">
+        <h1 style="font-size: 100px">
+            &#x1F6D2;
+        </h1>
+        <h1 class="m-20-ud">
+            E-commerce Checkout Template
+        </h1>
+        <p class="m-10-ud" style="font-size: 20px; max-width:600px; font-weight:300;">
+            Aplikasi web ini akan memungkinkan orang untuk melihat data dan mensimulasikan transaksi pembelian barang di E-commerce. 
+        </p>
+        <a href="/form" class="m-20-ud cta">Get Started</a>
+    </div>
+    
+
+@endsection
